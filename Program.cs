365105d19c5095a1f75924e7722e212f58496e99
Taylo <Taylo@MARIEL>
2025-06-18 11:27:08 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Implement a stack by using array with push and pop operations
+using System.Collections.Generic;
+
+namespace StackAssignment
+{
+   internal class StackArray
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(27);
+            stack.Push(37);
+            stack.Push(67);
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            stack.Pop();
+            Console.WriteLine("after the pop: ");
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"after peek: {stack.Peek()}");
+            Console.ReadKey();
+        }
+    }
+}
